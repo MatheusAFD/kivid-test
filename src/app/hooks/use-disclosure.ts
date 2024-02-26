21af07a20d0596d52ff052aeb2653d96784e6540
Matheus <matheus.daf@outlook.com>
2024-02-25 22:04:43 -0300

    :sparkles: Create custom hook to control modal state

---
+++ src/app/hooks/use-disclosure.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export function useDisclosure() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function onOpen() {
+    setIsOpen(true)
+  }
+
+  function onClose() {
+    setIsOpen(false)
+  }
+
+  return { isOpen, onOpen, onClose }
+}
